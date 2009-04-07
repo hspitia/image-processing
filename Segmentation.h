bddc019b54add1c2085ad925b74de51dee5166c4
unknown <luzma@.(none)>
2009-04-06 23:29:20 -0500

    Cambios Segementation hacia Otsu multinivel

--- Segmentation.h
+++ Segmentation.h
@@ -5,6 +5,7 @@
 #include <cmath>
 #include "Histogram.h"
 #include "Image.h"
+#include "utils/Array.h"
 
 using namespace std;
 
@@ -18,6 +19,7 @@ class Segmentation {
 		
 		static int thresholdingIsodata(Image * image);
 		static int thresholdingOtsu(Image * image);
+		static Array<int> multiThresholdingOtsu(Image * image, int const & numThresholds);
 		static Image segmentate(const Image & image, const int & threshold);
 		
 	
